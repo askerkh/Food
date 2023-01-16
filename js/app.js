1e8c1b8c8bfc33227af9e9c8becf9249b22837c8
Asker <a7ker.kh@gmail.com>
2023-01-16 18:13:27 +0300

    Added carousel slider with indicators on bottom

--- js/app.js
+++ js/app.js
@@ -166,13 +166,20 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getData('http://localhost:3000/menu')
-        .then(cards => {
-            cards.forEach(({img, altimg, title, descr, price}) => {
+    // getData('http://localhost:3000/menu')
+    //     .then(cards => {
+    //         cards.forEach(({img, altimg, title, descr, price}) => {
+    //             new FoodCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+    
+    axios.get('http://localhost:3000/menu')
+        .then(response => {
+            response.data.forEach(({img, altimg, title, descr, price}) => {
                 new FoodCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
-    
+
     // request's
     const forms = document.querySelectorAll('form');
 
@@ -249,7 +256,83 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 5000);
     }
 
-    fetch('http://localhost:3000/menu')
-        .then(response => response.json())
-        .then(json => console.log(json));
+    //slider
+    const slides = document.querySelectorAll('.offer__slide'),
+          slider = document.querySelector('.offer__slider'),
+          lSlideBtn = document.querySelector('.offer__slider-prev'),
+          rSlideBtn = document.querySelector('.offer__slider-next'),
+          current = document.querySelector('#current'),
+          total = document.querySelector('#total'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
+
+    total.textContent = (slides.length > 0 && slides.length < 10) ? `0${slides.length}` : slides.length;
+
+    let slideIndex = 1;
+    let offset = 0;
+
+    slidesWrapper.style.overflow = 'hidden';
+    
+    slidesField.style.width = `${slides.length * 100}%`;
+    slidesField.style.transition = `0.5s`;
+    slidesField.style.display = 'flex';
+
+    slides.forEach(slide => {
+        slide.style.width = width;    
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol');
+    indicators.classList.add('carousel-indicators');
+
+    slider.append(indicators);
+    const dots = [];
+    for(let i = 0; i < slides.length; ++i) {
+        const dot = document.createElement('li');
+        dot.classList.add('dot');
+
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    const parsedWidth = parseInt(width);
+
+    function translateSlides() {
+        slideIndex = offset / parsedWidth + 1;
+        current.textContent = (slideIndex > 0 && slideIndex < 10) ? `0${slideIndex}` : slideIndex;
+
+        dots.forEach(el => el.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = '1';
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+    }
+
+    translateSlides();
+
+    lSlideBtn.addEventListener('click', () => {
+        if(offset == 0) {
+            offset = parsedWidth * (slides.length - 1);
+        } else {
+            offset -= parsedWidth;
+        }
+        translateSlides();
+    });
+
+    rSlideBtn.addEventListener('click', () => {
+        if(offset == parsedWidth * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += parsedWidth;
+        }
+        translateSlides();
+    });
+
+    dots.forEach((el) => {
+        el.addEventListener('click', () => {
+            offset = (dots.indexOf(el) + 1)* parsedWidth - parsedWidth;
+            translateSlides();
+        });
+    });
 });
